f1746f0b7dd4d721af2f84aa9533dc755baec0a5
MakieTech <121760681+astigPree@users.noreply.github.com>
2024-11-28 15:47:10 +0800

    small changes

--- view/translator.php
+++ view/translator.php
@@ -67,12 +67,16 @@
                         <textarea id="textarea1"></textarea>
                     </div>
                     <div class="translator__col__action translator__col__speaker">
-                        <button class="fs-body-text te">
-                            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="25" x="0" y="0" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve">
+                        <button class="fs-body-text te" id="record-button">
+
+                        <svg height="25" width="25" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#ffffff" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <style type="text/css"> .st0{fill:#ffffff;} </style> <g> <path class="st0" d="M225.474,417.434c-14.997-0.018-30.099,5.766-41.536,17.183l-36.285,36.314 c-9.032,9.012-20.744,13.46-32.581,13.479c-11.838-0.02-23.542-4.467-32.582-13.479c-9.02-9.04-13.479-20.754-13.488-32.601 c0.009-11.827,4.468-23.541,13.488-32.581l-19.493-19.493c-14.357,14.348-21.584,33.278-21.574,52.074 c-0.02,18.806,7.217,37.737,21.574,52.094C77.345,504.763,96.265,512,115.072,512c18.806,0,37.736-7.237,52.084-21.575 l36.285-36.304c6.12-6.101,14.014-9.098,22.033-9.126c8.019,0.028,15.913,3.025,22.023,9.135 c6.464,6.464,9.451,14.921,9.108,23.388l27.55,1.194c0.707-15.828-5.06-32-17.145-44.075 C255.573,423.201,240.451,417.416,225.474,417.434z"></path> <path class="st0" d="M439.548,31.022c-39.12-39.12-101.219-41.23-142.859-6.396l149.255,149.265 C480.788,132.251,478.677,70.143,439.548,31.022z"></path> <path class="st0" d="M256.48,100.939L369.65,214.1c22.701-1.584,44.982-10.462,63.063-26.614L283.104,37.867 C266.943,55.939,258.075,78.22,256.48,100.939z"></path> <path class="st0" d="M51.637,347.47l71.472,71.472l222.494-201.578l-92.379-92.388L51.637,347.47z M274.723,210.482l-29.25,29.26 l-14.625-14.644l29.25-29.23L274.723,210.482z"></path> </g> </g></svg>
+
+                        <!-- <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="25" height="25" x="0" y="0" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512" xml:space="preserve">
                                 <g>
                                     <path d="M512 256c0 54.32-14.116 107.04-40.82 152.46-26.111 44.409-62.876 79.749-106.32 102.2a12 12 0 1 1-11.018-21.32C435.34 447.223 488 355.632 488 256S435.34 64.777 353.842 22.661a12 12 0 1 1 11.018-21.32c43.444 22.45 80.209 57.79 106.32 102.2C497.884 148.96 512 201.68 512 256ZM402.707 364.694c19.836-32.141 30.32-69.727 30.32-108.694s-10.484-76.553-30.32-108.694c-19.475-31.558-46.668-55.959-78.641-70.567a12 12 0 0 0-9.974 21.83c57.671 26.348 94.935 88.144 94.935 157.431s-37.264 131.083-94.935 157.431a12 12 0 1 0 9.974 21.83c31.973-14.608 59.166-39.009 78.641-70.567ZM354.054 256c0-48.215-29.751-90.675-72.35-103.256a12 12 0 0 0-6.8 23.018c32.47 9.589 55.148 42.585 55.148 80.238s-22.678 70.649-55.148 80.238a12 12 0 1 0 6.8 23.018c42.596-12.581 72.35-55.041 72.35-103.256ZM226.55 48.787v414.426a12 12 0 0 1-20.8 8.155L90.664 347.149H12a12 12 0 0 1-12-12V176.85a12 12 0 0 1 12-12h78.664L205.748 40.632a12 12 0 0 1 20.8 8.155Zm-24 30.609-97.842 105.609a12 12 0 0 1-8.8 3.845H24v134.3h71.905a12 12 0 0 1 8.8 3.845L202.55 432.6Z" fill="#efe8dc" opacity="1" data-original="#efe8dc"></path>
                                 </g>
-                            </svg>
+                            </svg> -->
+                        <span id="recoding-display" style=" display: none; color: #ffffff;"> Recording ... </span>
                         </button>
                     </div>
                 </div>
@@ -166,13 +170,31 @@
             english2.classList.remove('selected');
         });
 
-
         english2.addEventListener('click', function() {
             targetLanguage = 'en';
             english2.classList.add('selected');
             chabano2.classList.remove('selected');
             tagalog2.classList.remove('selected');
         });
+
+
+        let isRecording = false;
+
+        const recordDisplay = document.getElementById('recoding-display');
+        const recordButton = document.getElementById('record-button');
+        recordButton.addEventListener('click', function() {
+            if (isRecording) {
+                isRecording = false;
+                recordDisplay.style.display = 'none';
+                // stopRecording();
+            } else {
+                isRecording = true;
+                recordDisplay.style.display = 'inline';
+                // startRecording();
+            }
+        });
+
+
 
 
         // // Set default selection
